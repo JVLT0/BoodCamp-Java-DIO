0ce2ff0835ad825c3a8dbafccc32c543291ed916
Jão <joaovitorleal9999@gmail.com>
2024-06-18 01:13:58 -0300

    tipos de variaveis

---
+++ DIO-BoodCamp/src/dio/joao/sintaxjava/TiposVariaveis.java
@@ -0,0 +1,25 @@
+package dio.joao.sintaxjava;
+
+public class TiposVariaveis {
+    @SuppressWarnings("unused")
+    public static void main(String[] args) throws Exception {
+        
+        double salarioMinimo = 2500; //ISSO VALE COMO 2,500
+        double salarioMinimo1 = 2.500; //ISSO VALE COMO 2,5
+
+
+        //VARIAVEL
+
+        int numero = 5;
+        //PODE SER ALTERADO DURANTE O PROGRAMA
+
+        numero = 10;
+        System.out.println(numero);
+
+        //CONSTANTES
+
+        final double PI = 3.14;
+        //NÃO PODE SER ALTERADO DEPOIS DE DEFINIDO
+
+    }
+}
